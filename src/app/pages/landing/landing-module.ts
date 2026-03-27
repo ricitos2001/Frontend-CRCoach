95d3a5c64d36bf3917535e17f919d28e3108cef3
ricitos2001 <cesar2001ricitos@gmail.com>
2026-03-27 09:45:01 +0100

    Initial commit

---
+++ src/app/pages/landing/landing-module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '', loadComponent: () => import('./landing.page').then((m) => m.LandingPage), data: { title: 'Landing', breadcrumb: 'Landing' },
+      },
+    ]),
+  ],
+})
+export class LandingModule {}
