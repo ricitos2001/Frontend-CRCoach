679379293037bac011d3bcbc139f5dd86722ceb9
ricitos2001 <cesar2001ricitos@gmail.com>
2026-04-13 20:05:12 +0200

    Enhance dashboard and profile pages with user and profile loading states, and implement user loading by email

--- src/app/pages/dashboard/dashboard.page.ts
+++ src/app/pages/dashboard/dashboard.page.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, effect, OnInit } from '@angular/core';
 import { SidebarComponent } from '../../components/layout/sidebar/sidebar.component';
+import { UsersSignalStore } from '../../signal_stores/users.signal.store';
+import { PlayerProfileSignalStore } from '../../signal_stores/player-profile.signal.store';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +9,23 @@ import { SidebarComponent } from '../../components/layout/sidebar/sidebar.compon
   templateUrl: './dashboard.page.html',
   styleUrl: '../../../styles/styles.css',
 })
-export class DashboardPage {}
+export class DashboardPage implements OnInit {
+  constructor(
+    public usersStore: UsersSignalStore,
+    public profileStore: PlayerProfileSignalStore,
+  ) {
+    effect(() => {
+      const user = this.usersStore.user();
+      if (user && user.playerTag && user.playerTag.trim() !== '') {
+        this.profileStore.loadByTag(user.playerTag);
+      }
+      this.profileStore.profile();
+    });
+  }
+
+  ngOnInit(): void {
+    const email = localStorage.getItem('email');
+    if (!email) return;
+    this.usersStore.loadByEmail(email);
+  }
+}
