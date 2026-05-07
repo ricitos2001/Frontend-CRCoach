e0fa9ad4e9d90f9f8eb46d0518441bd55f91a0d7
ricitos2001 <cesar2001ricitos@gmail.com>
2026-05-07 17:19:31 +0200

    feat: Update footer and header components with social media links and click handlers

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -11,4 +11,34 @@ import { TranslatePipe } from '@ngx-translate/core';
 })
 export class FooterComponent {
   constructor(protected router: Router) {}
+
+  openDiscord($event: MouseEvent) {
+    $event.preventDefault();
+    window.open('https://discord.gg/y3uXnHjBK6', '_blank');
+  }
+
+  openIstagram($event: MouseEvent) {
+    $event.preventDefault();
+    window.open('https://www.instagram.com/ricitos2001/?hl=es%2F', '_blank');
+  }
+
+  openTwitter($event: MouseEvent) {
+    $event.preventDefault();
+    window.open('https://x.com/ricitos2001', '_blank');
+  }
+
+  openGithub($event: MouseEvent) {
+    $event.preventDefault();
+    window.open('https://github.com/ricitos2001', '_blank');
+  }
+
+  openLinkedin($event: MouseEvent) {
+    $event.preventDefault();
+    window.open('https://www.linkedin.com/in/cesar-gabriel-ucha-sousa-78386339a/', '_blank');
+  }
+
+  openKofi($event: MouseEvent) {
+    $event.preventDefault();
+    window.open('https://ko-fi.com/ricitos2001/tip', '_blank');
+  }
 }
